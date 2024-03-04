2ac7b5ba75aa2e5a9b3bef4ba1a3169e75a86224
Banghao Chi <banghao2@illinois.edu>
2024-03-03 21:44:30 -0600

    Finilized bluetooth connection

---
+++ server.py
@@ -0,0 +1,52 @@
+import bluetooth
+import picar_4wd as fc
+import json
+import threading
+import time
+
+def send_server_info():
+    server_info = {
+        "cpu_temperature": fc.cpu_temperature(),
+        "power_level": fc.power_read(),
+        "expected_speed": 5*(1-fc.power_read())
+    }
+    return json.dumps(server_info).encode()
+
+hostMACAddress = "dc:a6:32:92:06:d6"  # The address of Raspberry PI Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+port = 0
+backlog = 1
+size = 1024
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.bind((hostMACAddress, port))
+s.listen(backlog)
+print("listening on port ", port)
+
+try:
+    client, clientInfo = s.accept()
+    print("server recv from: ", clientInfo)
+    thread = threading.Thread(target=send_server_info, args=(client,))
+    thread.start()
+
+    while True:
+        data = client.recv(size)
+        if data:
+            print(data)
+            if "cpu" in str(data):
+                cpu_tem = str(fc.cpu_temperature())
+                message = "CPU temperature is " + cpu_tem + "Fahrenheit.\n"
+                client.send(message.encode())
+            elif "power" in str(data):
+                cpu_tem = str(fc.power_read())
+                message = "Power is at " + cpu_tem + ".\n"
+                client.send(message.encode())
+            else:
+                client.send(send_server_info() + "\n".encode())
+
+            client.send("Your data is: " + data + ".")
+
+
+except Exception as e:
+    print("Error:", e)
+    print("Closing socket")
+    client.close()
+    s.close()
